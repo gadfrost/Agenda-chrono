154184cb9064eca86c3732b684de319c2a89521c
gadfrost <goldengad72@Gmail.com>
2026-03-26 17:40:29 +0200

    Change cache name to 'stella-v4.0' and improve activation
    Updated cache name and added cache management during activation.

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'stella-v3';
+const CACHE_NAME = 'stella-v4.0';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
@@ -10,12 +11,22 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
