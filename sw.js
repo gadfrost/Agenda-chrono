73155f8549b5a68ad72d6053e9dd7f5729fa32f3
gadfrost <goldengad72@Gmail.com>
2026-03-26 19:48:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,15 +7,18 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
+// Installation : Mise en cache des fichiers essentiels
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Stella : Mise en cache des ressources');
       return cache.addAll(ASSETS);
     })
   );
   self.skipWaiting();
 });
 
+// Activation : Nettoyage des anciennes versions du cache
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -27,6 +30,7 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
+// Récupération : Servir depuis le cache si hors-ligne
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
