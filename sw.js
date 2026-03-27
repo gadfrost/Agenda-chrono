d5679b39c3696f87406a45be1a1ba613c788113c
gadfrost <goldengad72@Gmail.com>
2026-03-27 13:32:58 +0200

    v10

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE_NAME = 'stella-v10';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+const CACHE_NAME = 'stella-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
 });
 
 self.addEventListener('fetch', (e) => {
